30a266b791ac7836af800a8bcbf7b29718701621
ryanhammer <77473341+ryanhammer@users.noreply.github.com>
2024-01-09 02:09:16 +0000

    add day 1 work plus boilerplate readme from replit

---
+++ Day 1/main.py
@@ -0,0 +1,8 @@
+print('''Ryan
+January 8, 2024
+
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes per day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone (if needed) wherever I happen to be. No excuses for not coding from the middle of a field!
+I am feeling 😁
+You can follow my progress at replit.com/@ryanhammer''');
