95f082c47c85e1963665baf7be7e730b8409a0b9
ryanhammer <77473341+ryanhammer@users.noreply.github.com>
2024-01-10 01:22:53 +0000

    add day 2 work

---
+++ Day 2/main.py
@@ -0,0 +1,8 @@
+print("Greetings fair user! Please introduce yourself!")
+print()
+userName = input("What is your name? ")
+favoriteFood = input("What is your favorite food? ")
+favoriteMuscicalAct = input("Who is your favorite musician? ")
+userLocation = input("Where do you live? ")
+
+print("Just so I've got this, correct, your name is " + userName + ", your favorite food is " + favoriteFood + ", your favorite music is " + favoriteMuscicalAct + ", and you live in " + userLocation + ".")
