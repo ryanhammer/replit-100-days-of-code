99f205ef7e0d6add1872dc99e374cd1af57ca0f4
ryanhammer <77473341+ryanhammer@users.noreply.github.com>
2024-01-11 17:55:14 +0000

    add day 3 work

---
+++ Day 3/main.py
@@ -0,0 +1,8 @@
+foodType = input("What type of food do you want? ")
+plantType = input("What type of plant do you want? ")
+cookingMethod = input("What method of cooking do you want? ")
+burnedFood = input("Give a word describes burned food: ")
+householdItem = input("Give a household item: ")
+
+print(cookingMethod, foodType, "with", burnedFood, plantType, "on a bed of",
+      householdItem)
