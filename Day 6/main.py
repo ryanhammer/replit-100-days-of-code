7cd80a18a70f30d86549f88c2cebc0409842303e
ryanhammer <77473341+ryanhammer@users.noreply.github.com>
2024-01-19 02:19:13 +0000

    completed day 6 work

---
+++ Day 6/main.py
@@ -0,0 +1,15 @@
+# Day 6 Challenge
+print('SECURE LOGIN')
+username = input('Username > ')
+password = input('Password> ')
+
+if username == 'dorothy' and password == 'n0PlaceL1keHom3':
+  print('Greetings and Salutations Ms. Dorothy! Would you like to see Mr. Wizard today?')
+elif username == 'scarecrow' and password == 'brainsB4brawn':
+  print('Hey there Scarecrow! How is that diploma treating ya?')
+elif username == 'tin man' and password == 'allH3art':
+  print('Hello Mr. Tin Man! You are looking "heart-y" today, hehe...')
+elif username == 'cowardly lion' and password == 'IMBrave':
+  print('Hey there Lion! Keeping the forest in order?')
+else:
+  print('Get away from here! And take your little dog too!')
