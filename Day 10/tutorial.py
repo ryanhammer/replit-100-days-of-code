4681194e793ced72c5c08856aca234fd87fc96d9
ryanhammer <77473341+ryanhammer@users.noreply.github.com>
2024-01-21 22:19:52 +0000

    complete day 10 tutorial

---
+++ Day 10/tutorial.py
@@ -0,0 +1,49 @@
+adding = 4 + 3
+print(adding)
+
+subtraction = 8 - 9
+print(subtraction)
+
+multiplication = 4 * 32
+print(multiplication)
+
+division = 50 / 5
+print(division)
+
+# a number raised to the power of some number
+# in this example we raise 5 to the power of 2
+squared = 5**2
+print(squared)
+
+# remainder of a division
+modulo = 15 % 4
+print(modulo)
+
+# whole number division
+divisor = 15 // 2
+print(divisor)
+
+# Fix My Code
+# Before:
+👉 # Solve the following problems with my code
+# Your goal is to print the solution of all 3 calculations to the screen.
+
+# multiplication
+3.4 * 6.8
+
+# division
+2467 / 4673
+
+#raise 10 to the power of 2
+
+# print the remainder when 343 is divided by 4
+print("343 // 100")
+
+# After
+print(3.4 * 6.8)
+
+print(2467 / 4673)
+
+print(10**2)
+
+print(343 % 4)
