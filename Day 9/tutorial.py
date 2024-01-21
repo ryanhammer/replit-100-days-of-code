dab0ba7c7f0beae66f158bc6f588834fcb0d4c9b
ryanhammer <77473341+ryanhammer@users.noreply.github.com>
2024-01-21 22:09:32 +0000

    complete day 9 tutorial

---
+++ Day 9/tutorial.py
@@ -0,0 +1,20 @@
+myScore = int(input("Your score: "))
+if myScore > 100000:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+myPi = float(input("What is Pi to 3dp? "))
+if myPi == 3.142 :
+  print("Exactly!")
+else:
+  print("Try again 😭")
+
+# Fix My Code
+score = int(input("What was your score on the test? "))
+if score >= 80:
+  print("Not too shabby")
+elif score > 70:
+  print("Acceptable.")
+else:
+  print("Dude, you need to study more!")
